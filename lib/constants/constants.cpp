ec66e03e003c10955afa42ae13b4221fbb0fdbe2
Leo Nicolle <leo@linkurio.us>
2024-07-08 13:40:34 +0200

    click wheel pinout

--- lib/constants/constants.cpp
+++
@@ -1,7 +0,0 @@
-#include "constants.h"
-
-// semur
-// const String &serverURL = "http://192.168.8.137:3011/";
-// pothenee
-const String &serverURL = "http://192.168.8.105:3011/";
-
--- lib/constants/secrets.h
+++
@@ -1,9 +0,0 @@
-
-
-#ifndef SECRETS_H
-#define SECRETS_H
-
-extern const char *ssid;
-extern const char *password;
-
-#endif /* SECRETS_H */
